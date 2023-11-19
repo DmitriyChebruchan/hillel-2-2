3317ad12bdd134e6c6133522893d63e2abb02082
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-19 23:21:15 +0200

    creating library and books with script for demonstrating the way it works

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# hillel-2-2
+# Library Management System
+
+This Python-based Library Management System is designed to manage books and magazines efficiently. It incorporates features like iterators, generators, decorators, context managers, and object-oriented programming principles.
+
+## Classes
+
+### Book Model
+
+The `BookModel` class is defined using Pydantic to model book attributes such as title, author, and publication year.
+
+### Book Class
+
+The `Book` class represents a book instance and contains a method to return book information as a string.
+
+### Library Class
+
+The `Library` class manages a collection of books and magazines. It includes:
+
+- Methods to add, remove, and list books in the library.
+- Filtering books by author.
+- Saving and loading the library contents to/from a file.
+
+## Functionality
+
+- Creating instances of books and magazines.
+- Adding and removing items from the library.
+- Displaying the library contents.
+- Filtering books by author name.
+- Saving the library contents to a file and loading them back.
+
+## Usage
+
+1. Create instances of books and magazines using `BookModel`.
+2. Add items to the library using `Library.add_book()` or `Library.remove_book()` methods.
+3. Use `Library.list_books()` to display all items in the library.
+4. Filter books by author with `Library.books_by_author()`.
+5. Save the library contents to a file using `Library.save_to_file()` and load them back with `Library.load_from_file()`.
+
+## How to Run
+
+1. Clone this repository.
+```
+git clone https://github.com/DmitriyChebruchan/hillel-2-2
+```
+2. Install the required dependencies from requirements.txt.
+```
+pip install -r requirements.txt
+```
+3. Run the script containing the library management system.
---
+++ script.py
@@ -0,0 +1,59 @@
+from typing import List
+from issues_classes import Magazine, BookModel
+from library_classes import BookLibrary, MagazineLibrary
+
+# Creating instances of books and magazines
+book1 = BookModel(title="The Catcher in the Rye", author="J.D. Salinger", year=1951)
+book2 = BookModel(title="To Kill a Mockingbird", author="Harper Lee", year=1960)
+magazine1 = Magazine(title="Magazine A", author="Mag Author", year=2022)
+
+# Creating a BookLibrary and MagazineLibrary
+book_library = BookLibrary()
+magazine_library = MagazineLibrary()
+
+# Adding books and magazines to their respective libraries
+book_library.add_book(book1)
+book_library.add_book(book2)
+magazine_library.add_book(magazine1)
+
+# Displaying all books in the BookLibrary
+print("List of all books in BookLibrary:")
+book_library.list_books()
+
+# Displaying all magazines in the MagazineLibrary
+print("\nList of all magazines in MagazineLibrary:")
+magazine_library.list_books()
+
+# Displaying books by a specific author in BookLibrary
+print("\nBooks by author 'Harper Lee' in BookLibrary:")
+book_library.books_by_author('Harper Lee')
+
+# Saving BookLibrary contents to a file
+book_library.save_to_file('books.txt')
+
+# Removing a book from BookLibrary
+book_library.remove_book(book1)
+
+# Displaying books after removal in BookLibrary
+print("\nBooks after removal in BookLibrary:")
+book_library.list_books()
+
+# Loading books from a file to BookLibrary and displaying them
+book_library.load_from_file('books.txt')
+print("\nBooks after loading from file in BookLibrary:")
+book_library.list_books()
+
+# Saving MagazineLibrary contents to a file
+magazine_library.save_to_file('magazines.txt')
+
+# Removing a magazine from MagazineLibrary
+magazine_library.remove_book(magazine1)
+
+# Displaying magazines after removal in MagazineLibrary
+print("\nMagazines after removal in MagazineLibrary:")
+magazine_library.list_books()
+
+# Loading magazines from a file to MagazineLibra
+magazine_library.load_from_file('magazines.txt')
+print("\nMagazines after loading from file in MagazineLibrary:")
+magazine_library.list_books()
