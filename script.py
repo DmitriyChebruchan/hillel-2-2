3c60341dd9a67b3d458f20c0438d91aa534b1993
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-11-20 14:17:08 +0200

    refactoring

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ from issues_classes import Magazine, BookModel
 from library_classes import BookLibrary, MagazineLibrary
 
 # Creating instances of books and magazines
-book1 = BookModel(title="The Catcher in the Rye", author="J.D. Salinger", year=1951)
+book1 = BookModel(
+    title="The Catcher in the Rye", author="J.D. Salinger", year=1951
+)
 book2 = BookModel(title="To Kill a Mockingbird", author="Harper Lee", year=1960)
 magazine1 = Magazine(title="Magazine A", author="Mag Author", year=2022)
 
@@ -25,10 +27,10 @@ magazine_library.list_books()
 
 # Displaying books by a specific author in BookLibrary
 print("\nBooks by author 'Harper Lee' in BookLibrary:")
-book_library.books_by_author('Harper Lee')
+book_library.books_by_author("Harper Lee")
 
 # Saving BookLibrary contents to a file
-book_library.save_to_file('books.txt')
+book_library.save_to_file("books.txt")
 
 # Removing a book from BookLibrary
 book_library.remove_book(book1)
@@ -38,12 +40,12 @@ print("\nBooks after removal in BookLibrary:")
 book_library.list_books()
 
 # Loading books from a file to BookLibrary and displaying them
-book_library.load_from_file('books.txt')
+book_library.load_from_file("books.txt")
 print("\nBooks after loading from file in BookLibrary:")
 book_library.list_books()
 
 # Saving MagazineLibrary contents to a file
-magazine_library.save_to_file('magazines.txt')
+magazine_library.save_to_file("magazines.txt")
 
 # Removing a magazine from MagazineLibrary
 magazine_library.remove_book(magazine1)
@@ -53,6 +55,6 @@ print("\nMagazines after removal in MagazineLibrary:")
 magazine_library.list_books()
 
 # Loading magazines from a file to MagazineLibra
-magazine_library.load_from_file('magazines.txt')
+magazine_library.load_from_file("magazines.txt")
 print("\nMagazines after loading from file in MagazineLibrary:")
 magazine_library.list_books()
